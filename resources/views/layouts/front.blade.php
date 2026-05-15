f83652b67e89994e884c9473b7ebc7ef3c24cd71
Lee Ibrahim <leeibrah@gmail.com>
2026-05-15 13:36:17 +0300

    SEO

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -182,8 +182,10 @@
   @yield('schema')
 
   {{-- ====================================================== --}}
-  <link rel="icon" type="image/png" href="{{ asset('favicon.png') }}">
-  <link rel="apple-touch-icon" href="{{ asset('favicon.png') }}">
+  <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+  <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+  <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+  <link rel="apple-touch-icon" href="{{ asset('apple-touch-icon.png') }}">
   <link rel="stylesheet" href="{{ asset('css/cherub.css') }}">
   <script defer src="{{ asset('front/js/fontawesome/all.min.js') }}"></script>
 </head>
